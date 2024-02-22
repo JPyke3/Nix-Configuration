ebb87c88466c7a5768b334b30c56ee8577b62c29
JPyke3 <pyke.jacob1@gmail.com>
2024-02-22 10:50:08 +1000

    Added Tmux

--- home.nix
+++ home.nix
@@ -1,26 +1,4 @@
 { config, pkgs, ... }:
-let
-  better-vim-tmux-resizer = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "modern-tmux-theme";
-    version = "unstable-2023-01-06";
-    src = pkgs.fetchFromGitHub {
-      owner = "RyanMillerC";
-      repo = "better-vim-tmux-resizer";
-      rev = "a791fe5b4433ac43a4dad921e94b7b5f88751048";
-	  hash = "sha256-1uHcQQUnViktDBZt+aytlBF1ZG+/Ifv5VVoKSyM9ML0=";
-    };
-  };
-  tmux-powerline = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "modern-tmux-theme";
-    version = "unstable-2023-01-06";
-    src = pkgs.fetchFromGitHub {
-      owner = "erikw";
-      repo = "tmux-powerline";
-      rev = "fa164e9e491045299bf3dda1e18d507c4e3c5e27";
-	  hash = "sha256-DRu2rctev0jQmmoSLjk0YLVxl0rSnfiVzI9b/yoaJJE=";
-    };
-  };
-in
 {
   home.username = "jacobpyke";
   home.homeDirectory = "/home/jacobpyke";
@@ -33,6 +11,7 @@ in
 	pkgs.aria
 	pkgs.tmux
 	pkgs.nodejs_20 # LTS and Needed for Copilot
+	pkgs.direnv
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -83,20 +62,51 @@ in
 			fi
 
 			bindkey -s ^f "tmux-sessionizer\n"
+
+			eval "$(direnv hook zsh)"
 		'';
   };
 
   programs.tmux = {
 	enable = true;
-	plugins = with pkgs.tmuxPlugins; [
-		dracula
-		vim-tmux-navigator
-		better-vim-tmux-resizer
-		tmux-powerline
+	plugins = with pkgs; [
+		{ 
+			plugin = tmuxPlugins.dracula;
+ 			extraConfig =
+				''
+					# available plugins: battery, cpu-usage, git, gpu-usage, ram-usage, tmux-ram-usage, network, network-bandwidth, network-ping, ssh-session, attached-clients, network-vpn, weather, time, mpc, spotify-tui, kubernetes-context, synchronize-panes
+					set -g @dracula-show-empty-plugins false
+					set -g @dracula-plugins "git attached-clients battery weather time"
+					set -g @dracula-show-powerline true
+
+					# it can accept `hostname` (full hostname), `session`, `shortname` (short name), `smiley`, `window`, or any character. 
+					set -g @dracula-clients-minimum 1
+					set -g @dracula-show-left-icon session
+					set -g @dracula-show-fahrenheit false
+					set -g @dracula-show-location false
+				'';
+		}
+		tmuxPlugins.vim-tmux-navigator
+		{ plugin = tmuxPlugins.mkTmuxPlugin {
+			pluginName = "better-vim-tmux-resizer";
+			version = "unstable-2021-08-02";
+			rtpFilePath = "better-vim-tmux-resizer.tmux";
+			src = pkgs.fetchFromGitHub {
+			  owner = "RyanMillerC";
+			  repo = "better-vim-tmux-resizer";
+			  rev = "a791fe5b4433ac43a4dad921e94b7b5f88751048";
+			  hash = "sha256-1uHcQQUnViktDBZt+aytlBF1ZG+/Ifv5VVoKSyM9ML0=";
+			};
+		  }; }
 	];
 
 	extraConfig =
 		''
+		set -g default-terminal "xterm-256color"
+		set -ga terminal-overrides ",*256col*:Tc"
+		set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+		set-environment -g COLORTERM "truecolor"
+
 		# vim-like pane resizing  
 		bind -r C-k resize-pane -U
 		bind -r C-j resize-pane -D
@@ -121,17 +131,6 @@ in
 		unbind C-Right
 
 		set-option -g status-position top
-
-		# available plugins: battery, cpu-usage, git, gpu-usage, ram-usage, tmux-ram-usage, network, network-bandwidth, network-ping, ssh-session, attached-clients, network-vpn, weather, time, mpc, spotify-tui, kubernetes-context, synchronize-panes
-		set -g @dracula-show-empty-plugins false
-		set -g @dracula-plugins "git attached-clients battery weather time"
-		set -g @dracula-show-powerline true
-
-		# it can accept `hostname` (full hostname), `session`, `shortname` (short name), `smiley`, `window`, or any character. 
-		set -g @dracula-clients-minimum 1
-		set -g @dracula-show-left-icon session
-		set -g @dracula-show-fahrenheit false
-		set -g @dracula-show-location false
 		'';
 
   };
