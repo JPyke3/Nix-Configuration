d60420ac51584c6f36a9c85d0408796027fb02c4
JPyke3 <pyke.jacob1@gmail.com>
2024-02-27 16:51:06 +1000

    Home-Manager Rebuild: 208

--- programs/nvim/nvim.nix
+++ programs/nvim/nvim.nix
@@ -4,4 +4,5 @@
   ...
 }: {
   xdg.configFile."nvim/lua/personal_config/theme.lua".text = import ./theme.nix {inherit config;};
+  xdg.configFile."nvim/lua/plugin_config/lualine_config.lua".text = import ./lualine_config.nix {inherit pkgs;};
 }
