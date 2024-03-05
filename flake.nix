d2fdd2091a09e22c0d2df93b1e4248273c42103b
JPyke3 <pyke.jacob1@gmail.com>
2024-03-05 13:54:46 +1000

    Home-Manager Rebuild: 244

--- flake.nix
+++ flake.nix
@@ -8,7 +8,6 @@
     nur.url = "github:nix-community/nur";
     home-manager-unstable = {
       url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
