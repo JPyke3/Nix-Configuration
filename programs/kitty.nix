a87d189286e6ab59b4a23441baf6a984ab266680
JPyke3 <pyke.jacob1@gmail.com>
2024-03-14 11:52:47 +1000

    Home-Manager Rebuild: 35

--- programs/kitty.nix
+++ programs/kitty.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  	home.packages = with pkgs; [
+   		 (nerdfonts.override {fonts = ["FiraMono"];})
+	];
 	programs.kitty = {
 		enable = true;
+		font = "FiraMono Nerd Font";
 	};
 }
