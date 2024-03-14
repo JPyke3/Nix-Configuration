b21462b55795ebf69da3f2954a69137ec2cccd27
JPyke3 <pyke.jacob1@gmail.com>
2024-03-14 12:13:53 +1000

    Home-Manager Rebuild: 38

--- programs/kitty/kitty.nix
+++ programs/kitty/kitty.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }: let
+{ pkgs, config, ... }: let
   fontSize =
     if pkgs.stdenv.isDarwin
     then 16
     else 10;
 in {
-	xdg.configFile."kitty/themes/JacobsTheme.conf".text = import ./Jacobs-Theme.nix;
+	xdg.configFile."kitty/themes/JacobsTheme.conf".text = import ./Jacobs-Theme.nix {inherit config;};
 	programs.kitty = {
 		enable = true;
 		font = {
