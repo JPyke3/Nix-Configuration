32cc438b7c1056d579f6f978e1cd8cc4ee5ff69a
JPyke3 <pyke.jacob1@gmail.com>
2024-03-15 11:41:12 +1000

    Home-Manager Rebuild: 253

--- linux-home.nix
+++ linux-home.nix
@@ -34,6 +34,10 @@ in {
   home.file = {
   };
 
+  home.activation.setupEtc = config.lib.dag.entryAfter ["writeBoundary"] ''
+    /run/current-system/sw/bin/systemctl start --user sops-nix
+  '';
+
   imports = [
     ./programs/firefox.nix
     ./programs/hyprland.nix
