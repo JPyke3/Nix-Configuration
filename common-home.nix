116d5d9e7c45136c47a920d15c089f83790ea4bd
JPyke3 <pyke.jacob1@gmail.com>
2024-03-15 11:48:05 +1000

    Home-Manager Rebuild: 255

--- common-home.nix
+++ common-home.nix
@@ -46,7 +46,7 @@ in {
   sops = {
     defaultSopsFile = ./secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
-    age.keyFile = "/home/jacobpyke/.config/sops/age/keys.txt";
+    age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
 
     secrets = {
       "openai_api_key" = {};
