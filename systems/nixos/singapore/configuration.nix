bc831913cb741136103c0ea4b9ea372b55c7dda0
JPyke3 <pyke.jacob1@gmail.com>
2024-03-15 14:56:24 +1000

    Relocated nixos files

--- systems/nixos/singapore/configuration.nix
+++ systems/nixos/singapore/configuration.nix
@@ -10,7 +10,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    ./singapore/hardware-configuration.nix
+    ./hardware-configuration.nix
   ];
 
   nixpkgs.config.allowUnfree = true;
