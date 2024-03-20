c360316c6639a7955c0dce9e448e67db10924c86
JPyke3 <pyke.jacob1@gmail.com>
2024-03-20 21:20:42 +1000

    Added deck oled configs

---
+++ systems/nixos/japan/configuration.nix
@@ -0,0 +1,37 @@
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  imports = [
+    inputs.jovian.nixosModules.jovian
+  ];
+
+  jovian.devices.steamdeck.enable = true;
+
+  jovian.steam = {
+    enable = true;
+    autoStart = true;
+    desktopSession = "gnome";
+    user = "jacobpyke";
+  };
+
+  networking.hostName = "jacob-japan"; # Define your hostname.
+  networking.networkmanager.wifi.powersave = true;
+
+  services.xserver.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.pipewire.enable = true;
+
+  services.printing.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    amdgpu_top
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+  };
+}
---
+++ systems/nixos/japan/home.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  home.packages = [
+    pkgs.steam
+    pkgs.armcord
+  ];
+
+  imports = [
+    ../home.nix
+    ../../../programs/desktop/firefox.nix
+  ];
+
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+}
