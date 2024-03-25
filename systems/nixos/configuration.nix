cdb4d4a82ea9a00baaa215cb168e5518038e1be1
JPyke3 <pyke.jacob1@gmail.com>
2024-03-25 13:28:02 +1000

    Rebuild: NixOS: 84

--- systems/nixos/configuration.nix
+++ systems/nixos/configuration.nix
@@ -35,7 +35,7 @@
     defaultSopsFile = ../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
-    secrests.users.jacobpyke.password.neededForUsers = true;
+    secrets.users.jacobpyke.password.neededForUsers = true;
   };
 
   # Configure network proxy if necessary
