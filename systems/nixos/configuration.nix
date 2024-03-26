7324e6a0cd15a593b2d2c75afe99f89a397aeeaf
JPyke3 <pyke.jacob1@gmail.com>
2024-03-26 14:02:56 +1000

    Added tailscale

--- systems/nixos/configuration.nix
+++ systems/nixos/configuration.nix
@@ -13,12 +13,9 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  networking.networkmanager.enable = true;
   networking.nameservers = ["1.1.1.1" "1.0.0.1"]; # Cloudflare DNS
   networking.networkmanager.dns = "none";
   networking.dhcpcd.extraConfig = "nohook resolv.conf";
@@ -38,53 +35,32 @@
     secrets."users/jacobpyke/password".neededForUsers = true;
   };
 
-    fonts = {
-    fonts = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      source-code-pro
-      source-han-mono
-      source-han-sans
-      source-han-serif
-      wqy_zenhei
-    ];
-
-    fontDir.enable = true;
-    fontconfig.enable = true;
-	};
-
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  fonts = {
+  	fonts = with pkgs; [
+  	  noto-fonts
+  	  noto-fonts-cjk
+  	  noto-fonts-emoji
+  	  source-code-pro
+  	  source-han-mono
+  	  source-han-sans
+  	  source-han-serif
+  	  wqy_zenhei
+  	];
+  
+  	fontDir.enable = true;
+  	fontconfig.enable = true;
+  };
 
   nix.settings.experimental-features = "nix-command flakes";
 
   programs.zsh.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   users.defaultUserShell = pkgs.zsh;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.jacobpyke = {
     isNormalUser = true;
     home = "/home/jacobpyke";
-    extraGroups = ["wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
+    extraGroups = ["wheel" "networkmanager"];
     packages = with pkgs; [
       tree
     ];
@@ -94,8 +70,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
+    vim 
+	wget
     kitty
     wofi
     blueman
