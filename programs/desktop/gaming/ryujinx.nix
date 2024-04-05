eca99057d7ec145952e04fc79a2b8fe7dea861a7
JPyke3 <pyke.jacob1@gmail.com>
2024-04-05 15:13:17 +1000

    Rebuild: NixOS: 87

--- programs/desktop/gaming/ryujinx.nix
+++ programs/desktop/gaming/ryujinx.nix
@@ -1,5 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [
     ryujinx
   ];
+
+  sops.secrets."emulation/switch" = {
+    path = "${config.home.homeDirectory}/.config/Ryujinx/system/Prod.keys";
+  };
 }
