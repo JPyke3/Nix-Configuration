a91c546c42a4e623b4996b3d27f10392b8217113
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 21:17:47 +1000

    Rebuild: NixOS: 88

--- programs/desktop/gaming/ryujinx.nix
+++ programs/desktop/gaming/ryujinx.nix
@@ -11,5 +11,5 @@
     path = "${config.home.homeDirectory}/.config/Ryujinx/system/prod.keys";
   };
 
-  xdg.configFile."Ryujinx/bis/user/save" = config.lib.file.mkOutOfStoreSymlink "/home/jacobpyke/data/Games/Saves/Ryujinx/save"
+  xdg.configFile."Ryujinx/bis/user/save" = config.lib.file.mkOutOfStoreSymlink "/home/jacobpyke/data/Games/Saves/Ryujinx/save";
 }
