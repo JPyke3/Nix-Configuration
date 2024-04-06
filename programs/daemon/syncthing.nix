a0e6b22dfdd639979ab5450011514ed7a1cc474d
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 10:45:44 +1000

    Rebuild: NixOS: 88

--- programs/daemon/syncthing.nix
+++ programs/daemon/syncthing.nix
@@ -14,30 +14,30 @@
       overrideFolders = true; # overrides any folders added or deleted through the WebUI
       settings = {
         devices = {
-          "singapore" = { id = "CNOEUAD-NAKEO7K-VFEOW3J-GYDKRJL-VQTCJDX-VHKDQUP-HHVIGNW-U5QY5AW"; };
-		  "netherlands" = { id = "XQ4QCNY-G22ANRR-JBSR3SX-ODBHINE-OFS5NNF-IQQOAKW-KMSVRNA-OZLA5AQ"; };
+          "singapore" = {id = "CNOEUAD-NAKEO7K-VFEOW3J-GYDKRJL-VQTCJDX-VHKDQUP-HHVIGNW-U5QY5AW";};
+          "netherlands" = {id = "XQ4QCNY-G22ANRR-JBSR3SX-ODBHINE-OFS5NNF-IQQOAKW-KMSVRNA-OZLA5AQ";};
         };
         folders = {
           "Documents" = {
             path = "/home/jacobpyke/data/Documents";
             devices = ["singapore"];
           };
-		  "Downloads" = {
-		  	path = "/home/jacobpyke/data/Downloads";
-			devices = ["singapore", "netherlands"];
-		  };
+          "Downloads" = {
+            path = "/home/jacobpyke/data/Downloads";
+            devices = ["singapore" "netherlands"];
+          };
           "Game Saves" = {
             path = "/home/jacobpyke/data/Games/Saves";
-            devices = ["singapore", "netherlands"];
+            devices = ["singapore" "netherlands"];
           };
-		  "Game Roms" = {
-			path = "/home/jacobpyke/data/Games/Roms";
-			devices = ["singapore", "netherlands"];
-		  };
-		  "Game Firmware" = {
-			path = "/home/jacobpyke/data/Games/Firmware";
-			devices = ["singapore", "netherlands"];
-		  };
+          "Game Roms" = {
+            path = "/home/jacobpyke/data/Games/Roms";
+            devices = ["singapore" "netherlands"];
+          };
+          "Game Firmware" = {
+            path = "/home/jacobpyke/data/Games/Firmware";
+            devices = ["singapore" "netherlands"];
+          };
         };
       };
     };
