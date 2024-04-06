86283d11132c97ff8f1c610a8b1f74b35774d752
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 21:22:15 +1000

    Added game saves to syncthing

--- programs/desktop/gaming/ryujinx.nix
+++ programs/desktop/gaming/ryujinx.nix
@@ -11,5 +11,6 @@
     path = "${config.home.homeDirectory}/.config/Ryujinx/system/prod.keys";
   };
 
+  # Symlink to Syncthing Directory
   xdg.configFile."Ryujinx/bis/user/save".source = config.lib.file.mkOutOfStoreSymlink "/home/jacobpyke/data/Games/Saves/Ryujinx/save";
 }
