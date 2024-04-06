486eae68f913f2ae102252e09b0da129588e2832
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 10:23:24 +1000

    Rebuild: NixOS: 88

--- programs/daemon/syncthing.nix
+++ programs/daemon/syncthing.nix
@@ -4,19 +4,12 @@
   sops,
   ...
 }: {
-  sops.secrets."programs/syncthing/guiusername" = {};
-  sops.secrets."programs/syncthing/guipassword" = {};
-
   services = {
     syncthing = {
       enable = true;
       user = "jacobpyke";
       dataDir = "/home/jacobpyke/data";
       configDir = "/home/jacobpyke/.config/syncthing";
-      extraFlags = [
-        "--gui-user=$(cat /var/secrets/syncthing/guiusername)"
-        "--gui-password=$(cat /var/secrets/syncthing/guipassword)"
-      ];
     };
   };
 }
