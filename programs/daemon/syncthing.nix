ae53cef450d985a1004579fe4b99a99cb25bc538
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 14:22:35 +1000

    Rebuild: NixOS: 88

--- programs/daemon/syncthing.nix
+++ programs/daemon/syncthing.nix
@@ -63,6 +63,10 @@
             path = "/home/jacobpyke/data/Games/Firmware";
             devices = ["singapore"];
           };
+		  "Test New Folder" = {
+            path = "/home/jacobpyke/data/Test";
+			devices = ["singapore"]
+		  };
         };
       };
     };
