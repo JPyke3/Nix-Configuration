54209b355b08a634912d9aa7284cab527548a44c
JPyke3 <github@pyk.ee>
2024-04-06 18:40:21 +1000

    Rebuild: Nix Darwin: 4

--- systems/darwin/home.nix
+++ systems/darwin/home.nix
@@ -18,7 +18,7 @@ in {
   ];
 
   programs.zsh.initExtra = ''
-     	#make sure brew is on the path for M1
+   	#make sure brew is on the path for M1
     if [[ $(uname -m) == 'arm64' ]]; then
     	 eval "$(/opt/homebrew/bin/brew shellenv)"
     fi
