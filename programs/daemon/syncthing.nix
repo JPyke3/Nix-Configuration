08748b8602d8236234339800886edf3b2da6c972
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 11:03:05 +1000

    Rebuild: NixOS: 88

--- programs/daemon/syncthing.nix
+++ programs/daemon/syncthing.nix
@@ -14,22 +14,31 @@
       overrideFolders = true; # overrides any folders added or deleted through the WebUI
       settings = {
         devices = {
+          # Desktop
           "singapore" = {id = "CNOEUAD-NAKEO7K-VFEOW3J-GYDKRJL-VQTCJDX-VHKDQUP-HHVIGNW-U5QY5AW";};
+
+          # Seedbox
           "netherlands" = {
             addresses = [
               "tcp://merlion-direct.usbx.me:14568"
             ];
             id = "XQ4QCNY-G22ANRR-JBSR3SX-ODBHINE-OFS5NNF-IQQOAKW-KMSVRNA-OZLA5AQ";
           };
+
+          # iPhone
+          "korea" = {
+            id = "O44PLCJ-GQFQP2Z-WCQ5LWY-5AVPXQX-NV5CU3T-DYKD3D2-VVCZSZN-ORRTRQD";
+            autoAcceptFolders = "true";
+          };
         };
         folders = {
           "Documents" = {
             path = "/home/jacobpyke/data/Documents";
-            devices = ["singapore"];
+            devices = ["singapore" "korea"];
           };
           "Downloads" = {
             path = "/home/jacobpyke/data/Downloads";
-            devices = ["singapore" "netherlands"];
+            devices = ["singapore" "netherlands" "korea"];
           };
           "Game Saves" = {
             path = "/home/jacobpyke/data/Games/Saves";
