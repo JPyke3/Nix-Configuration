61eb902dc0efca4f1411246a350c89b9f731d353
JPyke3 <pyke.jacob1@gmail.com>
2024-04-06 10:33:58 +1000

    Rebuild: NixOS: 88

--- programs/daemon/syncthing.nix
+++ programs/daemon/syncthing.nix
@@ -10,6 +10,23 @@
       user = "jacobpyke";
       dataDir = "/home/jacobpyke/data";
       configDir = "/home/jacobpyke/.config/syncthing";
+      overrideDevices = true; # overrides any devices added or deleted through the WebUI
+      overrideFolders = true; # overrides any folders added or deleted through the WebUI
+      settings = {
+        devices = {
+          "singapore" = {id = "CNOEUAD-NAKEO7K-VFEOW3J-GYDKRJL-VQTCJDX-VHKDQUP-HHVIGNW-U5QY5AW";};
+        };
+        folders = {
+          "Documents" = {
+            path = "/home/jacobpyke/data/Documents";
+            devices = ["singapore"];
+          };
+          "Game Saves" = {
+            path = "/home/jacobpyke/data/Games/Saves";
+            devices = ["singapore"];
+          };
+        };
+      };
     };
   };
 }
