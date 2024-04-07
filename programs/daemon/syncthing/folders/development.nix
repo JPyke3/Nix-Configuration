50e95dd8a2d36ca41fb60439b14c6aaf8d23eb0d
JPyke3 <pyke.jacob1@gmail.com>
2024-04-08 09:24:50 +1000

    Rebuild: NixOS: 89

--- programs/daemon/syncthing/folders/development.nix
+++ programs/daemon/syncthing/folders/development.nix
@@ -1,4 +1,5 @@
-{...}: {
+{config, ...}: {
+  home.folder."Development".source = config.lib.file.mkOutOfStoreSymlink "/home/jacobpyke/data/Development"
   services.syncthing.settings.folders."Development" = {
     path = "/home/jacobpyke/data/Development";
     devices = ["singapore" "netherlands" "korea" "germany"];
