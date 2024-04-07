af06e3b6043a38cf3e72feff9c115bd0a63bade5
JPyke3 <github@pyk.ee>
2024-04-08 06:07:56 +1000

    Rebuild: Nix Darwin: 9

--- systems/nixos/singapore/syncthing.nix
+++ systems/nixos/singapore/syncthing.nix
@@ -3,6 +3,7 @@
   imports = [
     ../../../programs/daemon/syncthing/folders/documents.nix
     ../../../programs/daemon/syncthing/folders/downloads.nix
+    ../../../programs/daemon/syncthing/folders/development.nix
     ../../../programs/daemon/syncthing/folders/gamesaves.nix
     ../../../programs/daemon/syncthing/folders/gameroms.nix
     ../../../programs/daemon/syncthing/folders/gamefirmware.nix
