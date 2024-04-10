41af7d30867062d6df2b3e7b289972494402e05b
JPyke3 <pyke.jacob1@gmail.com>
2024-04-10 11:48:33 +1000

    Rebuild: NixOS: 91

--- programs/desktop/gaming/suyu.nix
+++ programs/desktop/gaming/suyu.nix
@@ -2,4 +2,8 @@
   environment.systemPackages = [
     inputs.jpyke3-suyu.packages.x86_64-linux.suyu-dev
   ];
+
+  sops.secrets."emulation/switch" = {
+    path = "${config.home.homeDirectory}/.local/share/suyu/keys/prod.keys";
+  };
 }
