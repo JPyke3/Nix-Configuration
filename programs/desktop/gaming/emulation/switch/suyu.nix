905ee1c72d3990794e4a4c9ecbf511e57c0dd755
JPyke3 <pyke.jacob1@gmail.com>
2024-04-10 15:43:42 +1000

    Rebuild: NixOS: 93

--- programs/desktop/gaming/emulation/switch/suyu.nix
+++ programs/desktop/gaming/emulation/switch/suyu.nix
@@ -14,4 +14,6 @@
   home.file."${config.home.homeDirectory}/.local/share/suyu/keys/prod.keys".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.local/emulation/keys/prod.keys";
 
   home.file."${config.home.homeDirectory}/.local/share/suyu/nand/user/save".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/data/Games/Saves/Yuzu/save";
+
+  home.file."${config.home.homeDirectory}/.local/share/suyu/load".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/data/Games/Mods/Yuzu";
 }
