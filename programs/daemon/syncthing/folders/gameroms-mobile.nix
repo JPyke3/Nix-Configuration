2797d46380360a8939087c1abe95b6c5d82bfd08
JPyke3 <pyke.jacob1@gmail.com>
2024-04-18 08:08:57 +1000

    Rebuild: NixOS: 106

---
+++ programs/daemon/syncthing/folders/gameroms-mobile.nix
@@ -0,0 +1,6 @@
+{...}: {
+  services.syncthing.settings.folders."Game Roms (Mobile Friendly)" = {
+    path = "/home/jacobpyke/data/Games/Mobile Roms";
+    devices = ["singapore" "germany" "japan" "korea"];
+  };
+}
