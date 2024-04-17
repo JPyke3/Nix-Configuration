bf2cbe34acdf30e0559f8355e1413c6b719a3fbc
JPyke3 <pyke.jacob1@gmail.com>
2024-04-18 08:08:00 +1000

    Rebuild: NixOS: 105

--- systems/nixos/singapore/syncthing.nix
+++ systems/nixos/singapore/syncthing.nix
@@ -6,6 +6,7 @@
     ../../../programs/daemon/syncthing/folders/development.nix
     ../../../programs/daemon/syncthing/folders/gamesaves.nix
     ../../../programs/daemon/syncthing/folders/gameroms.nix
+    ../../../programs/daemon/syncthing/folders/gameroms-mobile.nix
     ../../../programs/daemon/syncthing/folders/gamemods.nix
     ../../../programs/daemon/syncthing/folders/gamefirmware.nix
     ../../../programs/daemon/syncthing/folders/gamesteamgrids.nix
