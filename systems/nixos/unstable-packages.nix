c8cc21f1bdd5354ca3bafa601369a26b8988861b
JPyke3 <pyke.jacob1@gmail.com>
2024-04-19 08:44:50 +1000

    Rebuild: NixOS: 108

--- systems/nixos/unstable-packages.nix
+++ systems/nixos/unstable-packages.nix
@@ -1,9 +1,9 @@
 {
   inputs,
-  system,
+  nixpkgs,
   ...
 }: let
-  pkgs_unstable = inputs.unstable.legacyPackages.${system};
+  pkgs_unstable = inputs.unstable.legacyPackages.${nixpkgs.lib.nixosSystem.system};
 in {
   environment.systemPackages = with pkgs_unstable; [
     nh
