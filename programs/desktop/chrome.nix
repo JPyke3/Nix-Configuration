9f854c9361ae7c0d5176803bad684a2852a4f358
JPyke3 <pyke.jacob1@gmail.com>
2024-04-24 11:27:18 +1000

    Rebuild: NixOS: 116

--- programs/desktop/chrome.nix
+++ programs/desktop/chrome.nix
@@ -5,7 +5,7 @@
       {id = "nngceckbapebfimnlniiiahkandclblb";} #Bitwarden
       {id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";} #uBlock Origin
       {id = "likgccmbimhjbgkjambclfkhldnlhbnn";} #Yomitan
-      {id = "lkhiljgmbeecmljiogckofcalncmfnfo";} #Migaku
+      {id = "acpchjgielgmkgkplljakcibfbjjppbk";} #Migaku
       {id = "mnjggcdmjocbbbhaepdhchncahnbgone";} #Sponsorblock
       {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";} #Dark Reader
       {id = "dbepggeogbaibhgnhhndojpepiihcmeb";} #Vimium
