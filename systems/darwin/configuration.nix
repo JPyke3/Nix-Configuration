08234796cc0e615ffdeb0c467d4876a0252c8e24
JPyke3 <github@pyk.ee>
2024-05-06 12:00:23 +1000

    Rebuild: Nix Darwin: 13

--- systems/darwin/configuration.nix
+++ systems/darwin/configuration.nix
@@ -64,6 +64,11 @@
 		};
 	};
 
+	  system.activationScripts.postUserActivation.text = ''
+    # Following line should allow us to avoid a logout/login cycle
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
+
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
   # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
