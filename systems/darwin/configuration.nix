bf1334595bffb367adf42e58d5614faeea168b07
JPyke3 <github@pyk.ee>
2024-05-06 11:59:27 +1000

    Rebuild: Nix Darwin: 12

--- systems/darwin/configuration.nix
+++ systems/darwin/configuration.nix
@@ -57,10 +57,10 @@
 			show-recents = false;
 
 			# Disable Hover actions
-			wvous-bl-corner = "1";
-			wvous-br-corner = "1";
-			wvous-tl-corner = "1";
-			wvous-tr-corner = "1";
+			wvous-bl-corner = 1;
+			wvous-br-corner = 1;
+			wvous-tl-corner = 1;
+			wvous-tr-corner = 1;
 		};
 	};
 
