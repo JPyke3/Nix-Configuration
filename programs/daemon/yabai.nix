6c48c8e396a44ffb21e3bde2b88bb3f7634d7c69
JPyke3 <github@pyk.ee>
2024-05-06 15:25:06 +1000

    Rebuild: Nix Darwin: 27

--- programs/daemon/yabai.nix
+++ programs/daemon/yabai.nix
@@ -1,10 +1,11 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./skhd/skhd.nix
     ./sketchybar/sketchybar.nix
   ];
   services.yabai = {
     enable = true;
+    package = pkgs.yabai;
     enableScriptingAddition = true;
     config = {
       layout = "bsp";
