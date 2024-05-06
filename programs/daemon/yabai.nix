1d2013a763cc3f45db2f2fcf5c8a32573941da0a
JPyke3 <github@pyk.ee>
2024-05-06 17:06:15 +1000

    Rebuild: Nix Darwin: 39

--- programs/daemon/yabai.nix
+++ programs/daemon/yabai.nix
@@ -1,10 +1,17 @@
-{...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  pkgs_unstable = import inputs.unstable.legacyPackages.${pkgs.system};
+in {
   imports = [
     ./skhd/skhd.nix
     ./sketchybar/sketchybar.nix
   ];
   services.yabai = {
     enable = true;
+    package = pkgs.yabai;
     enableScriptingAddition = true;
     config = {
       layout = "bsp";
