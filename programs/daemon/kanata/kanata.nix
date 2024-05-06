e0b6d08384ef41c77a969e9f432d21801f9b5096
JPyke3 <github@pyk.ee>
2024-05-07 09:11:34 +1000

    Rebuild: Nix Darwin: 64

--- programs/daemon/kanata/kanata.nix
+++ programs/daemon/kanata/kanata.nix
@@ -17,25 +17,11 @@ in {
     source = ./keyboard.kbd;
   };
 
-  environment.launchDaemons."com.jpyke3.kanata" = {
-    enable = true;
-    text = ''
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-          <key>Label</key>
-          <string>com.example.mylaunchdaemon</string>
-          <key>ProgramArguments</key>
-          <array>
-              <string>${nur.repos.jpyke3.kanata-bin}/bin/kanata</string>
-              <string>--cfg</string>
-              <string>/etc/keyboard.kbd</string>
-          </array>
-          <key>RunAtLoad</key>
-          <true/>
-      </dict>
-      </plist>
-    '';
+  launchd.daemons.kanata = {
+    command = "exec ${nur.repos.jpyke3.kanata-bin}/bin/kanata --cfg /etc/keyboard.kbd";
+    serviceConfig = {
+      KeepAlive = true;
+      RunAtLoad = true;
+    };
   };
 }
