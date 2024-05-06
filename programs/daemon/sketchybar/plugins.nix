102fa1a06b04ec2c46a3ee949e80dfeae4bbae12
JPyke3 <github@pyk.ee>
2024-05-06 16:49:03 +1000

    Rebuild: Nix Darwin: 37

--- programs/daemon/sketchybar/plugins.nix
+++ programs/daemon/sketchybar/plugins.nix
@@ -1,58 +1,67 @@
 # Note, this is to be used as a home-manager module
 {...}: {
-  xdg.configFile."sketchybar/plugins/battery.sh".text = ''
-    #!/bin/sh
+  xdg.configFile."sketchybar/plugins/battery.sh" = {
+    text = ''
+      #!/bin/sh
 
-    PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
-    CHARGING="$(pmset -g batt | grep 'AC Power')"
+      PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
+      CHARGING="$(pmset -g batt | grep 'AC Power')"
 
-    if [ "$PERCENTAGE" = "" ]; then
-      exit 0
-    fi
+      if [ "$PERCENTAGE" = "" ]; then
+        exit 0
+      fi
 
-    case "''${PERCENTAGE}" in
-      9[0-9]|100) ICON=""
-      ;;
-      [6-8][0-9]) ICON=""
-      ;;
-      [3-5][0-9]) ICON=""
-      ;;
-      [1-2][0-9]) ICON=""
-      ;;
-      *) ICON=""
-    esac
+      case "''${PERCENTAGE}" in
+        9[0-9]|100) ICON=""
+        ;;
+        [6-8][0-9]) ICON=""
+        ;;
+        [3-5][0-9]) ICON=""
+        ;;
+        [1-2][0-9]) ICON=""
+        ;;
+        *) ICON=""
+      esac
 
-    if [[ "$CHARGING" != "" ]]; then
-      ICON=""
-    fi
+      if [[ "$CHARGING" != "" ]]; then
+        ICON=""
+      fi
 
-    # The item invoking this script (name $NAME) will get its icon and label
-    # updated with the current battery status
-    sketchybar --set "$NAME" icon="$ICON" label="''${PERCENTAGE}%"
-  '';
+      # The item invoking this script (name $NAME) will get its icon and label
+      # updated with the current battery status
+      sketchybar --set "$NAME" icon="$ICON" label="''${PERCENTAGE}%"
+    '';
+    executable = true;
+  };
 
-  xdg.configFile."sketchybar/plugins/clock.sh".text = ''
-    #!/bin/sh
+  xdg.configFile."sketchybar/plugins/clock.sh" = {
+    text = ''
+      #!/bin/sh
 
-    # The $NAME variable is passed from sketchybar and holds the name of
-    # the item invoking this script:
-    # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+      # The $NAME variable is passed from sketchybar and holds the name of
+      # the item invoking this script:
+      # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-    sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
-  '';
+      sketchybar --set "$NAME" label="$(date '+%d/%m %H:%M')"
+    '';
+    executable = true;
+  };
 
-  xdg.configFile."sketchybar/plugins/front_app.sh".text = ''
-    #!/bin/sh
+  xdg.configFile."sketchybar/plugins/front_app.sh" = {
+    text = ''
+      #!/bin/sh
 
-    # Some events send additional information specific to the event in the $INFO
-    # variable. E.g. the front_app_switched event sends the name of the newly
-    # focused application in the $INFO variable:
-    # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
+      # Some events send additional information specific to the event in the $INFO
+      # variable. E.g. the front_app_switched event sends the name of the newly
+      # focused application in the $INFO variable:
+      # https://felixkratz.github.io/SketchyBar/config/events#events-and-scripting
 
-    if [ "$SENDER" = "front_app_switched" ]; then
-      sketchybar --set "$NAME" label="$INFO"
-    fi
-  '';
+      if [ "$SENDER" = "front_app_switched" ]; then
+        sketchybar --set "$NAME" label="$INFO"
+      fi
+    '';
+    executable = true;
+  };
 
   xdg.configFile."sketchybar/plugins/space.sh".text = ''
     #!/bin/sh
