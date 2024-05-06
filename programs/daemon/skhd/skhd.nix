6f41e843d68f3f5202f0c8eb09482e11027ecc3d
JPyke3 <github@pyk.ee>
2024-05-06 17:19:10 +1000

    Rebuild: Nix Darwin: 41

--- programs/daemon/skhd/skhd.nix
+++ programs/daemon/skhd/skhd.nix
@@ -2,7 +2,11 @@
   services.skhd = {
     enable = true;
     skhdConfig = ''
-      cmd - return : "/Users/jacobpyke/Applications/Home Manager Apps/Kitty.app/Contents/MacOS/kitty" --single-instance -d ~
+         cmd - return : "/Users/jacobpyke/Applications/Home Manager Apps/Kitty.app/Contents/MacOS/kitty" --single-instance -d ~
+      option - 1 : yabai -m space --focus 1
+      option - 2 : yabai -m space --focus 2
+      option - 3 : yabai -m space --focus 3
+      option - 4 : yabai -m space --focus 4
     '';
   };
 }
