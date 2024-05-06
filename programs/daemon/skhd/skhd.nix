18fcd20a414d7ebe5a9e6bc458c427c2cf81f3f7
JPyke3 <github@pyk.ee>
2024-05-06 16:17:35 +1000

    Rebuild: Nix Darwin: 35

--- programs/daemon/skhd/skhd.nix
+++ programs/daemon/skhd/skhd.nix
@@ -2,7 +2,7 @@
   services.skhd = {
     enable = true;
     skhdConfig = ''
-      cmd - return : /Users/jacobpyke/Applications/Home Manager Apps/Kitty.app/Contents/MacOS/kitty --single-instance -d ~
+      cmd - return : "/Users/jacobpyke/Applications/Home Manager Apps/Kitty.app/Contents/MacOS/kitty" --single-instance -d ~
     '';
   };
 }
