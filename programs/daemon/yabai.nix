e032439a0aa046897b6ea674f980deeb1c94f685
JPyke3 <github@pyk.ee>
2024-05-07 11:44:38 +1000

    Rebuild: Nix Darwin: 97

--- programs/daemon/yabai.nix
+++ programs/daemon/yabai.nix
@@ -13,14 +13,10 @@ in {
 
       # Focus follows mouse
       focus_follows_mouse = "autoraise";
-      mouse_follows_focus = "on";
 
       window_shadow = "float";
 
       external_bar = "main:20:0";
     };
-    extraConfig = ''
-      yabai -m rule --add app="^Firefox$" mouse_follows_focus=off
-    '';
   };
 }
