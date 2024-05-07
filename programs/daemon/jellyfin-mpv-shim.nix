4f8830776bff4480f792419fd55d86e63826b72b
JPyke3 <github@pyk.ee>
2024-05-07 13:07:07 +1000

    Rebuild: Nix Darwin: 106

--- programs/daemon/jellyfin-mpv-shim.nix
+++ programs/daemon/jellyfin-mpv-shim.nix
@@ -1,13 +1,12 @@
-{ ... }:
-{
-	launchd.agents.jellyfinmpvshim = {
-		command = "/Users/jacobpyke/Library/Python/3.9/bin/jellyfin-mpv-shim"
-		serviceConfig = {
-			KeepAlive = true;
-			RunAtLoad = true;
-			StandardErrorPath = "/tmp/jellyfin-mpv-shim.err";
-			StandardOutPath = "/tmp/jellyfin-mpv-shim.out";
-			ProcessType = "Interactive";
-		};
-	};
+{...}: {
+  launchd.agents.jellyfinmpvshim = {
+    command = "/Users/jacobpyke/Library/Python/3.9/bin/jellyfin-mpv-shim";
+    serviceConfig = {
+      KeepAlive = true;
+      RunAtLoad = true;
+      StandardErrorPath = "/tmp/jellyfin-mpv-shim.err";
+      StandardOutPath = "/tmp/jellyfin-mpv-shim.out";
+      ProcessType = "Interactive";
+    };
+  };
 }
