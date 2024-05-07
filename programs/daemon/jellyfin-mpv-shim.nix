3f2071f8f7a77ff5adacb4509144cc2654569cd9
JPyke3 <github@pyk.ee>
2024-05-07 13:28:38 +1000

    Rebuild: Nix Darwin: 113

--- programs/daemon/jellyfin-mpv-shim.nix
+++ programs/daemon/jellyfin-mpv-shim.nix
@@ -1,6 +1,9 @@
 {...}: {
   launchd.user.agents.jellyfinmpvshim = {
     command = "/usr/bin/python3 /Users/jacobpyke/Library/Python/3.9/bin/jellyfin-mpv-shim";
+    environment = {
+      PATH = "/Users/jacobpyke/.nix-profile/bin/";
+    };
     serviceConfig = {
       UserName = "jacobpyke";
       KeepAlive = true;
