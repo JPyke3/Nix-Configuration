fe17b3643151c283a98d071f870ad8f9026e8d1c
JPyke3 <github@pyk.ee>
2024-05-07 10:28:15 +1000

    Rebuild: Nix Darwin: 86

--- programs/daemon/kanata/kanata.nix
+++ programs/daemon/kanata/kanata.nix
@@ -17,8 +17,8 @@ in {
     source = ./keyboard.kbd;
   };
 
-  launchd.daemons.kanata = {
-    command = "${nur.repos.jpyke3.kanata-bin}/bin/kanata --cfg /etc/keyboard.kbd";
+  launchd.agents.kanata = {
+    command = "sudo ${nur.repos.jpyke3.kanata-bin}/bin/kanata --cfg /etc/keyboard.kbd";
     serviceConfig = {
       RunAtLoad = true;
       StandardErrorPath = "/var/log/kanata.err.log";
