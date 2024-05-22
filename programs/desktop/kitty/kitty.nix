177751d107ab95bd3a8fca277acaa6071cf20e31
JPyke3 <github@pyk.ee>
2024-05-23 08:33:18 +0900

    Rebuild: Nix Darwin: 154

--- programs/desktop/kitty/kitty.nix
+++ programs/desktop/kitty/kitty.nix
@@ -2,9 +2,15 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  fontSize =
+    if pkgs.stdenv.isDarwin
+    then 16
+    else 10;
+in {
   programs.kitty = {
     enable = true;
     shellIntegration.enableZshIntegration = true;
   };
+  font.size = fontSize;
 }
