0c36a7a8befc95b46e76c4107e6f7218e6001f21
JPyke3 <github@pyk.ee>
2024-05-27 20:26:17 +0900

    Rebuild: Nix Darwin: 160

--- systems/stylix.nix
+++ systems/stylix.nix
@@ -7,7 +7,6 @@ in {
   stylix.enable = true;
 
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
-  stylix.image = ../wallpapers/macos-wallpaper.png;
 
   stylix.fonts = {
     monospace = {
