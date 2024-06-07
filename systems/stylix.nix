ce8644727386aed1a78437c82a5e6f6ee98010f4
JPyke3 <github@pyk.ee>
2024-06-07 11:14:35 +0900

    Rebuild: Nix Darwin: 161

--- systems/stylix.nix
+++ systems/stylix.nix
@@ -9,21 +9,21 @@ in {
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-frappe.yaml";
   stylix.image = ../wallpapers/macos-wallpaper.png;
 
-  #  stylix.fonts = {
-  #    monospace = {
-  #      package = pkgs.nerdfonts.override {fonts = ["FiraMono"];};
-  #      name = "FiraMono Nerd Font";
-  #    };
-  #    sansSerif = {
-  #      package = pkgs.noto-fonts-cjk;
-  #      name = "Noto Sans CJK";
-  #    };
-  #    serif = {
-  #      package = pkgs.noto-fonts-cjk;
-  #      name = "Noto Serif CJK";
-  #    };
-  #    sizes = {
-  #      terminal = fontSize;
-  #    };
-  #  };
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerdfonts.override {fonts = ["FiraMono"];};
+      name = "FiraMono Nerd Font";
+    };
+    sansSerif = {
+      package = pkgs.noto-fonts-cjk;
+      name = "Noto Sans CJK";
+    };
+    serif = {
+      package = pkgs.noto-fonts-cjk;
+      name = "Noto Serif CJK";
+    };
+    sizes = {
+      terminal = fontSize;
+    };
+  };
 }
