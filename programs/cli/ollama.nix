f9fc9fc6197603fb412e372741c0948cb3536e1c
JPyke3 <pyke.jacob1@gmail.com>
2024-07-08 09:57:44 +1000

    Rebuild: NixOS: 138

--- programs/cli/ollama.nix
+++ programs/cli/ollama.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
   services.ollama = {
     enable = true;
-    acceleration = "rocm";
+    environmentVariables = {
+      HCC_AMDGPU_TARGET = "gfx1100"; # used to be necessary, but doesn't seem to anymore
+    };
+    rocmOverrideGfx = "10.3.1";
   };
 }
