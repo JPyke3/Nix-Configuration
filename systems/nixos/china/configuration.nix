d74e33d00f263eadfa0cffd478a341f63152c6fd
JPyke3 <github@pyk.ee>
2024-07-16 20:38:06 +1000

    Rebuild: NixOS: 1

--- systems/nixos/china/configuration.nix
+++ systems/nixos/china/configuration.nix
@@ -23,13 +23,6 @@
   # Set your time zone.
   time.timeZone = "Australia/Brisbane";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
   # Configure keymap in X11
   services.xserver = {
     layout = "au";
