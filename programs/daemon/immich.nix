1fe3cf010b4f658954b0b79b872d3fe6caa46aff
JPyke3 <github@pyk.ee>
2024-07-17 10:44:54 +1000

    Rebuild: NixOS: 1

--- programs/daemon/immich.nix
+++ programs/daemon/immich.nix
@@ -1,6 +1,9 @@
-{pkgs, outputs, ...} @ args: {
-
-  nixpkgs.overlays = [ outputs.immich ]
+{
+  pkgs,
+  outputs,
+  ...
+} @ args: {
+  nixpkgs.overlays = [outputs.immich];
 
   imports = [
     "${args.inputs.immich}/nixos/modules/services/web-apps/immich.nix"
