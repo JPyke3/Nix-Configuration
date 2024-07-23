84ca3d11e9d425700efbc48a302b37ffb1105c96
JPyke3 <github@pyk.ee>
2024-07-23 13:55:35 +1000

    Rebuild: Nix Darwin: 232

--- programs/cli/spicetify.nix
+++ programs/cli/spicetify.nix
@@ -1,17 +1,14 @@
-{ pkgs, ... }:
-{
-programs.spicetify =
-   let
-     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-   in
-   {
-     enable = true;
-     enabledExtensions = with spicePkgs.extensions; [
-       adblock
-       hidePodcasts
-       shuffle # shuffle+ (special characters are sanitized out of extension names)
-     ];
-     theme = spicePkgs.themes.catppuccin;
-     colorScheme = "mocha";
-   }
+{pkgs, ...}: {
+  programs.spicetify = let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  in {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      hidePodcasts
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+    ];
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+  };
 }
