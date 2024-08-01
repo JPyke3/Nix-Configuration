adf5268c57bb7bdd73a2c1c7ca399b30aeac3994
JPyke3 <github@pyk.ee>
2024-08-01 20:56:08 +1000

    Rebuild: NixOS: 3

--- programs/daemon/firefly-iii.nix
+++ programs/daemon/firefly-iii.nix
@@ -6,6 +6,7 @@
 
   services.firefly-iii = {
     enable = true;
+    enableNginx = true;
     settings = {
       APP_KEY_FILE = "/mypool/documents/firefly-iii/appkey.txt";
       APP_ENV = "production";
