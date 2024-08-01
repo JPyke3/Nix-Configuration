fc11ee93a98fb718186f580083777207e8494014
JPyke3 <github@pyk.ee>
2024-08-01 19:08:24 +1000

    Rebuild: NixOS: 2

--- programs/daemon/firefly-iii.nix
+++ programs/daemon/firefly-iii.nix
@@ -1,14 +1,15 @@
 {config, ...}: {
   sops.secrets."programs/firefly/appKey" = {
-    path = "${config.home.homeDirectory}/.secrets/firefly/appkey.txt";
+    path = "${config.users.users.jacobpyke.home}/.secrets/firefly/appkey.txt";
   };
+
   services.firefly-iii = {
     enable = true;
     dataDir = "/mypool/documents/firefly-iii";
     user = "firefly";
     group = "firefly";
     settings = {
-      APP_KEY_FILE = "${config.home.homeDirectory}/.secrets/firefly/appkey.txt";
+      APP_KEY_FILE = "${config.users.users.jacobpyke.home}/.secrets/firefly/appkey.txt";
     };
   };
 }
