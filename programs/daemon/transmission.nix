5d64f994a44afd8ae56cdb30a02c4253c5a7519b
JPyke3 <github@pyk.ee>
2024-08-02 17:50:11 +1000

    Rebuild: NixOS: 4

--- programs/daemon/transmission.nix
+++ programs/daemon/transmission.nix
@@ -3,6 +3,12 @@
     enable = true;
     group = "media-server";
     openFirewall = true;
-    settings.download-dir = "/mypool/downloads";
+    openRPCPort = true; #Open firewall for RPC
+    settings = {
+      #Override default settings
+      rpc-bind-address = "0.0.0.0"; #Bind to own IP
+      rpc-whitelist = "127.0.0.1,10.0.0.1"; #Whitelist your remote machine (10.0.0.1 in this example)
+      download-dir = "/mypool/downloads";
+    };
   };
 }
