636b8e161855dd2965a7f3a01682e0c126dc91dc
JPyke3 <github@pyk.ee>
2024-08-02 21:52:55 +1000

    Rebuild: NixOS: 4

--- programs/daemon/transmission.nix
+++ programs/daemon/transmission.nix
@@ -39,7 +39,6 @@
     settings = {
       #Override default settings
       rpc-bind-address = "0.0.0.0"; #Bind to own IP
-      rpc-whitelist = "127.0.0.1,100.114.95.59,192.168.*.*"; #Whitelist your remote machine (10.0.0.1 in this example)
       download-dir = "/mypool/downloads";
     };
   };
