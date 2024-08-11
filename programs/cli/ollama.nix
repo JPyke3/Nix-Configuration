f5686fc054572de2e1325846860a1ac034d968af
JPyke3 <pyke.jacob1@gmail.com>
2024-08-12 09:33:59 +1000

    Rebuild: NixOS: 154

--- programs/cli/ollama.nix
+++ programs/cli/ollama.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   services.ollama = {
     enable = true;
-	acceleration = "rocm";
-	rocmOverrideGfx = "11.0.0";
+    acceleration = "rocm";
+    rocmOverrideGfx = "11.0.0";
   };
 }
