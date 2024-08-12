06e6249023fcf89ec0df7476b94d5900e6437382
JPyke3 <pyke.jacob1@gmail.com>
2024-08-12 11:45:24 +1000

    Added docker

--- systems/nixos/singapore/configuration.nix
+++ systems/nixos/singapore/configuration.nix
@@ -7,6 +7,7 @@
     ./hardware-configuration.nix
     ./syncthing.nix
     ../../../programs/daemon/altserver.nix
+	../../../programs/deemon/docker.nix
   ];
 
   networking.hostName = "jacob-singapore"; # Define your hostname.
