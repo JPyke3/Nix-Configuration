91bf8e8fb8055d84d32acf88f798eaad4cac6dda
JPyke3 <github@pyk.ee>
2024-08-22 08:44:53 +1000

    Rebuild: NixOS: 20

--- programs/daemon/pihole.nix
+++ programs/daemon/pihole.nix
@@ -30,7 +30,7 @@
       # expose DNS & the web interface on unpriviledged ports on all IP addresses of the host
       # check the option descriptions for more information
       dnsPort = 5335;
-      webPort = 8080;
+      webPort = 69024;
     };
     piholeConfig.ftl = {
       # assuming that the host has this (fixed) IP and should resolve "pi.hole" to this address
