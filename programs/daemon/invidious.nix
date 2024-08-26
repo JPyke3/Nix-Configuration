440cd4714aa340bae0f662743ee791a6ad41d9fa
JPyke3 <github@pyk.ee>
2024-08-27 09:57:39 +1000

    Rebuild: NixOS: 23

--- programs/daemon/invidious.nix
+++ programs/daemon/invidious.nix
@@ -10,6 +10,8 @@
   services.invidious = {
     enable = true;
     port = 4664;
+    nginx.enable = true;
+    http3-ytproxy.enable = true;
     settings.db = {
       user = "invidious";
       dbname = "invidious";
