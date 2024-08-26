2f88f06ef9c5faf3c466ea5cc0dc75f0d78d8cce
JPyke3 <github@pyk.ee>
2024-08-27 09:51:34 +1000

    Rebuild: NixOS: 22

--- programs/daemon/invidious.nix
+++ programs/daemon/invidious.nix
@@ -1,11 +1,15 @@
-{sops, config, ...}: {
-  sops.secrets."programs/invidious/password" = {}
+{
+  sops,
+  config,
+  ...
+}: {
+  sops.secrets."programs/invidious/password" = {};
 
   services.invidious = {
     enable = true;
     port = 4664;
-	database = {
-		passwordFile = config.sops.secrets."programs/invidious/password".path;
-	};
+    database = {
+      passwordFile = config.sops.secrets."programs/invidious/password".path;
+    };
   };
 }
