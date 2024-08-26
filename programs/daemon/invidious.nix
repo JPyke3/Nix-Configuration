359d8cc3fb4521b2608dbe32162aa0503d954667
JPyke3 <github@pyk.ee>
2024-08-27 09:53:51 +1000

    Rebuild: NixOS: 23

--- programs/daemon/invidious.nix
+++ programs/daemon/invidious.nix
@@ -5,9 +5,15 @@
 }: {
   sops.secrets."programs/invidious/password" = {};
 
+  # ervices.invidious.settings.db.user must match services.invidious.settings.db.dbname
+
   services.invidious = {
     enable = true;
     port = 4664;
+    settings.db = {
+      user = "invidious";
+      dbname = "invidious";
+    };
     database = {
       passwordFile = config.sops.secrets."programs/invidious/password".path;
     };
