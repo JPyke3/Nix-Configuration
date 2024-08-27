963b7356ddfb42edcf877d20e0450146b4e92f0e
JPyke3 <github@pyk.ee>
2024-08-27 14:19:40 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nextcloud.nix
+++ programs/daemon/nextcloud.nix
@@ -2,5 +2,12 @@
   services.nextcloud = {
     enable = true;
     home = /mypool/documents/nextcloud;
+	hostName = "jacob-china.tail264a8.ts.net"
   };
-}
+
+  services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
+	  forceSSL = true;
+	  sslCertificate = /mypool/documents/Tailscale-Certs/jacob-china.tail264a8.ts.net.crt;
+	  sslCertificateKey = /mypool/documents/Tailscale-Certs/jacob-china.tail264a8.ts.net.key;
+  };
+};
