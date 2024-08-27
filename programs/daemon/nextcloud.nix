26fc1730b94920de011de0e1929f4c01d89730d6
JPyke3 <github@pyk.ee>
2024-08-28 08:32:23 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nextcloud.nix
+++ programs/daemon/nextcloud.nix
@@ -35,5 +35,34 @@
     forceSSL = true;
     sslCertificate = /mypool/documents/Tailscale-Certs/jacob-china.tail264a8.ts.net.crt;
     sslCertificateKey = /mypool/documents/Tailscale-Certs/jacob-china.tail264a8.ts.net.key;
+    "^~ /.well-known" = {
+      priority = 9000;
+      extraConfig = ''
+        absolute_redirect off;
+        location ~ ^/\\.well-known/(?:carddav|caldav)$ {
+          return 301 /nextcloud/remote.php/dav;
+        }
+        location ~ ^/\\.well-known/host-meta(?:\\.json)?$ {
+          return 301 /nextcloud/public.php?service=host-meta-json;
+        }
+        location ~ ^/\\.well-known/(?!acme-challenge|pki-validation) {
+          return 301 /nextcloud/index.php$request_uri;
+        }
+        try_files $uri $uri/ =404;
+      '';
+    };
+    "/nextcloud/" = {
+      priority = 9999;
+      extraConfig = ''
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-NginX-Proxy true;
+        proxy_set_header X-Forwarded-Proto http;
+        proxy_pass http://127.0.0.1:8080/; # tailing / is important!
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_redirect off;
+      '';
+    };
   };
 }
