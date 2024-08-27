962f262113331ed0f54b50ac5ee301ce37d0aace
JPyke3 <github@pyk.ee>
2024-08-27 10:32:20 +1000

    Rebuild: NixOS: 24

--- programs/daemon/searxng.nix
+++ programs/daemon/searxng.nix
@@ -1,12 +1,16 @@
-{sops, config, ...}: {
+{
+  sops,
+  config,
+  ...
+}: {
   sops.secrets."programs/searx/envfile" = {};
   services.searx = {
     enable = true;
-	environmentFile = config.sops.secrets."programs/searx/envfile".path;
-	settings = {
-		server.port = 2082;
-		server.bind_address = "0.0.0.0";
-		server.secret_key = "@SEARX_SECRET_KEY@"
-	};
+    environmentFile = config.sops.secrets."programs/searx/envfile".path;
+    settings = {
+      server.port = 2082;
+      server.bind_address = "0.0.0.0";
+      server.secret_key = "@SEARX_SECRET_KEY@";
+    };
   };
 }
