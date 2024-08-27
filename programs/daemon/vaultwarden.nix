4c3d9beb1afe840095bd4460fb0d173adb0574f2
JPyke3 <github@pyk.ee>
2024-08-27 11:21:55 +1000

    Rebuild: NixOS: 24

--- programs/daemon/vaultwarden.nix
+++ programs/daemon/vaultwarden.nix
@@ -6,8 +6,7 @@
   sops.secrets."programs/vaultwarden/envfile" = {};
   services.vaultwarden = {
     enable = true;
-    dbBackend = "postgresql";
-    backupDir = "/mypool/documents/vaultwarden/backup";
+    # backupDir = "/mypool/documents/vaultwarden/backup";
     environmentFile = config.sops.secrets."programs/vaultwarden/envfile".path;
   };
 }
