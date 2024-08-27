62128fa531013c80914289decc027a3269647a5d
JPyke3 <github@pyk.ee>
2024-08-27 14:21:49 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nextcloud.nix
+++ programs/daemon/nextcloud.nix
@@ -3,7 +3,11 @@
     enable = true;
     home = /mypool/documents/nextcloud;
     hostName = "jacob-china.tail264a8.ts.net";
-  };
+   };
+
+	nixpkgs.config.permittedInsecurePackages = [
+┃                 "nextcloud-27.1.11"                                      
+┃               ];
 
   services.nginx.virtualHosts.${config.services.nextcloud.hostName} = {
     forceSSL = true;
