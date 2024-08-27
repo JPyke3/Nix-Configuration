0b7f03c9459e44f40fde8b0c1ba8008d177b3e7d
JPyke3 <github@pyk.ee>
2024-08-27 13:21:23 +1000

    Rebuild: NixOS: 24

--- programs/daemon/vaultwarden.nix
+++ programs/daemon/vaultwarden.nix
@@ -4,15 +4,6 @@
   ...
 }: {
   sops.secrets."programs/vaultwarden/envfile" = {};
-  security.acme.defaults.email = "jacob@pyk.ee";
-  security.acme.acceptTerms = true;
-  services.nginx.virtualHosts."vaultwarden.jacob-china.tail264a8.ts.net" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}";
-    };
-  };
   services.vaultwarden = {
     enable = true;
     # backupDir = "/mypool/documents/vaultwarden/backup";
