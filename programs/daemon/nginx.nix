cca42a2f48d86492a41385011a8657919b52fc09
JPyke3 <github@pyk.ee>
2024-08-28 14:11:51 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nginx.nix
+++ programs/daemon/nginx.nix
@@ -13,7 +13,7 @@ in {
 
     virtualHosts = {
       "nextcloud.${domain}" = {
-        forceSSL = true;
+        # forceSSL = true;
         useACMEHost = "${domain}";
         locations."/" = {
           proxyPass = "http://127.0.0.1:8080";
