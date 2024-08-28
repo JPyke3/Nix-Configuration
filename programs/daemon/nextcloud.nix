a4c0913b3a8bc4d5a6e386efb53a5c9abd654065
JPyke3 <github@pyk.ee>
2024-08-28 15:17:26 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nextcloud.nix
+++ programs/daemon/nextcloud.nix
@@ -13,7 +13,7 @@
       "jacob-china.tail264a8.ts.net"
     ];
     enable = true;
-    hostName = "127.0.0.1";
+    hostName = "nextcloud.pyk.ee";
     config.adminpassFile = config.sops.secrets."programs/nextcloud/adminpass".path;
   };
 
