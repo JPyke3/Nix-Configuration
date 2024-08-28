a07d6da16e1dbe6bab9a3e7b0512b613860f26fe
JPyke3 <github@pyk.ee>
2024-08-28 14:41:26 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nginx.nix
+++ programs/daemon/nginx.nix
@@ -7,13 +7,19 @@
 in {
   services.nginx = {
     enable = true;
-    # recommendedProxySettings = true;
-    # recommendedTlsSettings = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
 
     virtualHosts = {
       "nextcloud.${domain}" = {
-        # forceSSL = true;
-        # useACMEHost = "${domain}";
+	    listen = [
+			{
+				addr = "0.0.0.0";
+				port = "80":
+			};
+		];
+        forceSSL = true;
+        useACMEHost = "${domain}";
         locations."/" = {
           proxyPass = "http://127.0.0.1:8080";
         };
