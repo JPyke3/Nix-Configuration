f790f0551ccb59ae20a5d36579fdcd958a345fb0
JPyke3 <github@pyk.ee>
2024-08-28 13:41:32 +1000

    Added acme

---
+++ programs/daemon/acme.nix
@@ -0,0 +1,21 @@
+{...}: {
+  sops.secrets."acme/route53" = {};
+
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "jacob@pyk.ee";
+      dnsProvider = "route53";
+      dnsPropagationCheck = true;
+      dnsResolver = "1.1.1.1:53";
+    };
+    certs = {
+      "${domain}" = {
+        extraDomainNames = ["*.${domain}"];
+        dnsProvider = "route53";
+        credentialsFile = config.sops.secrets."acme/route53".path;
+        group = "nginx";
+      };
+    };
+  };
+}
