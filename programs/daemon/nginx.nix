bbffc1530b2031ff5a9c84bd4ea4e4c6d5a9fbf4
JPyke3 <github@pyk.ee>
2024-08-28 14:09:45 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nginx.nix
+++ programs/daemon/nginx.nix
@@ -6,38 +6,19 @@
   domain = "pyk.ee";
   tailscaleDomain = "jacob-china.tail264a8.ts.net"; # Replace with your actual Tailscale domain
 in {
-  services.caddy = {
+  services.nginx = {
     enable = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
     virtualHosts = {
       "nextcloud.${domain}" = {
-        extraConfig = ''
-          reverse_proxy localhost:8080
-        '';
-      };
-      "invidious.${domain}" = {
-        extraConfig = ''
-          reverse_proxy localhost:3000
-        '';
+        forceSSL = true;
+        useACMEHost = "${domain}";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:8080";
+        };
       };
     };
-  };
-
-  # Ensure Caddy uses Pi-hole for DNS resolution
-  systemd.services.caddy.environment = {
-    "DNS_SERVER" = "127.0.0.1";
-  };
-
-  networking = {
-    # Use Pi-hole as the primary DNS server
-    nameservers = ["127.0.0.1"];
-    # Disable DHCP's DNS server propagation
-    dhcpcd.extraConfig = "nohook resolv.conf";
-    # Prevent NetworkManager from overwriting resolv.conf
-    networkmanager.dns = "none";
-  };
-
-  networking.firewall = {
-    allowedTCPPorts = [80 443];
-    checkReversePath = "loose"; # Recommended for Tailscale
   };
 }
