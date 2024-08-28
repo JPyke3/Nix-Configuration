6b431cd4901a3b85f3c53ef9457ea5a85d5307b4
JPyke3 <github@pyk.ee>
2024-08-28 15:19:29 +1000

    Rebuild: NixOS: 24

--- programs/daemon/nginx.nix
+++ programs/daemon/nginx.nix
@@ -15,7 +15,7 @@ in {
         forceSSL = true;
         useACMEHost = "${domain}";
         locations."/" = {
-          proxyPass = "http://127.0.0.1:8080";
+          proxyPass = "http://0.0.0.0:8080";
         };
       };
     };
