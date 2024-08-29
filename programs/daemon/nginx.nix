cd588c153d238c979942587694f5fb65dda40980
JPyke3 <github@pyk.ee>
2024-08-30 09:38:39 +1000

    Rebuild: NixOS: 26

--- programs/daemon/nginx.nix
+++ programs/daemon/nginx.nix
@@ -18,109 +18,121 @@ in {
         forceSSL = true;
         useACMEHost = "${domain}";
 
+        # Increase max upload size
         extraConfig = ''
           client_max_body_size 512M;
         '';
 
         locations = {
           "/" = {
-            priority = 1;
             extraConfig = ''
               add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;" always;
-            '';
-            headers = {
-              "Referrer-Policy" = "no-referrer";
-              "X-Content-Type-Options" = "nosniff";
-              "X-Frame-Options" = "SAMEORIGIN";
-              "X-Permitted-Cross-Domain-Policies" = "none";
-              "X-Robots-Tag" = "noindex, nofollow";
-              "X-XSS-Protection" = "1; mode=block";
-            };
-            root = "/var/lib/nextcloud";
-            index = "index.php index.html /index.php$request_uri";
-          };
+              # Set headers for Nextcloud
+              add_header Referrer-Policy                   "no-referrer"       always;
+              add_header X-Content-Type-Options            "nosniff"           always;
+              add_header X-Frame-Options                   "SAMEORIGIN"        always;
+              add_header X-Permitted-Cross-Domain-Policies "none"              always;
+              add_header X-Robots-Tag                      "noindex, nofollow" always;
+              add_header X-XSS-Protection                  "1; mode=block"     always;
 
-          "= /" = {
-            priority = 2;
-            extraConfig = ''
-              if ( $http_user_agent ~ ^DavClnt ) {
-                return 302 /remote.php/webdav/$is_args$args;
+              # Remove X-Powered-By, which is an information leak
+              fastcgi_hide_header X-Powered-By;
+
+              # Path to the root of your Nextcloud installation
+              root /var/lib/nextcloud;
+
+              # Specify how to handle directories
+              index index.php index.html /index.php$request_uri;
+
+              # Rule borrowed from `.htaccess` to handle Microsoft DAV clients
+              location = / {
+                if ( $http_user_agent ~ ^DavClnt ) {
+                  return 302 /remote.php/webdav/$is_args$args;
+                }
               }
-            '';
-          };
 
-          "= /robots.txt" = {
-            priority = 3;
-            extraConfig = ''
-              allow all;
-              log_not_found off;
-              access_log off;
-            '';
-          };
+              location = /robots.txt {
+                allow all;
+                log_not_found off;
+                access_log off;
+              }
 
-          "^~ /.well-known" = {
-            priority = 4;
-            extraConfig = ''
-              location = /.well-known/carddav { return 301 /remote.php/dav/; }
-              location = /.well-known/caldav  { return 301 /remote.php/dav/; }
-              location /.well-known/acme-challenge    { try_files $uri $uri/ =404; }
-              location /.well-known/pki-validation    { try_files $uri $uri/ =404; }
-              return 301 /index.php$request_uri;
-            '';
-          };
+              # Make a regex exception for `/.well-known` so that clients can still
+              # access it despite the existence of the regex rule
+              location ^~ /.well-known {
+                # The rules in this block are an adaptation of the rules
+                # in `.htaccess` that concern `/.well-known`.
 
-          "~ ^/(?:build|tests|config|lib|3rdparty|templates|data)(?:$|/)" = {
-            priority = 5;
-            return = "404";
-          };
+                location = /.well-known/carddav { return 301 /remote.php/dav/; }
+                location = /.well-known/caldav  { return 301 /remote.php/dav/; }
 
-          "~ ^/(?:\.|autotest|occ|issue|indie|db_|console)" = {
-            priority = 6;
-            return = "404";
-          };
+                location /.well-known/acme-challenge    { try_files $uri $uri/ =404; }
+                location /.well-known/pki-validation    { try_files $uri $uri/ =404; }
 
-          "~ \\.php(?:$|/)" = {
-            priority = 7;
-            extraConfig = ''
-              fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-              set $path_info $fastcgi_path_info;
-              try_files $fastcgi_script_name =404;
-              include ${config.services.nginx.package}/conf/fastcgi_params;
-              fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-              fastcgi_param PATH_INFO $path_info;
-              fastcgi_param HTTPS on;
-              fastcgi_param modHeadersAvailable true;
-              fastcgi_param front_controller_active true;
-              fastcgi_pass unix:/run/phpfpm/nextcloud.sock;
-              fastcgi_intercept_errors on;
-              fastcgi_request_buffering off;
-              fastcgi_max_temp_file_size 0;
-            '';
-          };
+                # Let Nextcloud's API for `/.well-known` URIs handle all other
+                # requests by passing them to the front-end controller.
+                return 301 /index.php$request_uri;
+              }
 
-          "~ \\.(?:css|js|mjs|svg|gif|ico|png|webp|woff|woff2|ttf|map)$" = {
-            priority = 8;
-            extraConfig = ''
-              try_files $uri /index.php$request_uri;
-              add_header Cache-Control "public, max-age=15778463";
-              access_log off;
-            '';
-          };
+              # Rules borrowed from `.htaccess` to hide certain paths from clients
+              location ~ ^/(?:build|tests|config|lib|3rdparty|templates|data)(?:$|/)  { return 404; }
+              location ~ ^/(?:\.|autotest|occ|issue|indie|db_|console)                { return 404; }
 
-          "~ /\\.ht" = {
-            priority = 9;
-            return = "404";
-          };
+              # Ensure this block, which passes PHP files to the PHP process, is above the blocks
+              # which handle static assets (as seen below). If this block is not declared first,
+              # then Nginx will encounter an infinite rewriting loop when it prepends `/index.php`
+              # to the URI, resulting in a HTTP 500 error response.
+              location ~ \.php(?:$|/) {
+                # Required for legacy support
+                rewrite ^/(?!index|remote|public|cron|core\/ajax\/update|status|ocs\/v[12]|updater\/.+|ocs-provider\/.+|.+\/richdocumentscode(_arm64)?\/proxy) /index.php$request_uri;
 
-          "^~ /push" = {
-            priority = 10;
-            proxyPass = "http://127.0.0.1:7867";
-            extraConfig = ''
-              proxy_http_version 1.1;
-              proxy_set_header Upgrade $http_upgrade;
-              proxy_set_header Connection "Upgrade";
-              proxy_set_header Host $host;
-              proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+                set $path_info $fastcgi_path_info;
+
+                try_files $fastcgi_script_name =404;
+
+                include ${config.services.nginx.package}/conf/fastcgi_params;
+                fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+                fastcgi_param PATH_INFO $path_info;
+                fastcgi_param HTTPS on;
+
+                fastcgi_param modHeadersAvailable true;         # Avoid sending the security headers twice
+                fastcgi_param front_controller_active true;     # Enable pretty urls
+                fastcgi_pass unix:/run/phpfpm/nextcloud.sock;
+
+                fastcgi_intercept_errors on;
+                fastcgi_request_buffering off;
+
+                fastcgi_max_temp_file_size 0;
+              }
+
+              # Serve static files
+              location ~ \.(?:css|js|mjs|svg|gif|ico|png|webp|woff|woff2|ttf|map)$ {
+                try_files $uri /index.php$request_uri;
+                add_header Cache-Control "public, max-age=15778463";
+                access_log off;     # Optional: Don't log access to assets
+
+                location ~ \.woff2?$ {
+                  try_files $uri /index.php$request_uri;
+                  expires 7d;
+                  access_log off;
+                }
+              }
+
+              location ~ /\.ht {
+                deny all;
+              }
+
+              location ^~ /push {
+                proxy_pass http://127.0.0.1:7867;
+                proxy_http_version 1.1;
+                proxy_set_header Upgrade $http_upgrade;
+                proxy_set_header Connection "Upgrade";
+                proxy_set_header Host $host;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+              }
+
+              try_files $uri $uri/ /index.php$request_uri;
             '';
           };
         };
