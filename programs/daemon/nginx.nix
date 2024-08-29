74068a09bf5681972af6068bfa43506a0baf7151
JPyke3 <github@pyk.ee>
2024-08-29 16:58:32 +1000

    Rebuild: NixOS: 26

--- programs/daemon/nginx.nix
+++ programs/daemon/nginx.nix
@@ -105,6 +105,13 @@ in {
           proxyPass = "http://127.0.0.1:8000";
         };
       };
+      "immich.${domain}" = {
+        forceSSL = true;
+        useACMEHost = "${domain}";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:3001";
+        };
+      };
       "firefly.${domain}" = {
         forceSSL = true;
         useACMEHost = "${domain}";
