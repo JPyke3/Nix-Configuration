6f7cb8f31c3a3e98b127d1187e0440f17a24fea9
JPyke3 <github@pyk.ee>
2024-09-30 09:32:38 +1000

    Rebuild: NixOS: 33

--- programs/daemon/invidious.nix
+++ programs/daemon/invidious.nix
@@ -10,6 +10,10 @@
     nginx.enable = true;
     port = 4664;
     domain = "invidious.pyk.ee";
+    settings = {
+      po_token = "MnTRqoG8F3o_e9GS4_4wv7WlvuZP0YDHf1RR6kHqikcBy1N4QfKa-c32_51yZMuaGN_Zs0eR8kHvMqQv0-UiGtifGM2fBEAnfHrvG08MbuOOJoV0FeNSPT9QiTVqr6TMLmQfuCygbePTr7tnsVXB3QjmKzVC4A==";
+      visitor_data = "CgstWmhuWUx6dWhlQSjxxee3BjIKCgJBVRIEGgAgUg%3D%3D";
+    };
     settings.db = {
       user = "invidious";
       dbname = "invidious";
