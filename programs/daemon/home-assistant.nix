ef35be5133a33a50986ec621b0a28f70781e6f46
JPyke3 <github@pyk.ee>
2025-01-23 08:14:51 +1000

    Rebuild: NixOS: 33

--- programs/daemon/home-assistant.nix
+++ programs/daemon/home-assistant.nix
@@ -1,21 +1,27 @@
 {...}: {
-  services.home-assistant = {
-    enable = true;
-    openFirewall = true;
-    extraComponents = [
-      # Components required to complete the onboarding
-      "esphome"
-      "met"
-      "radio_browser"
-    ];
-    config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.homeassistant = {
+      volumes = ["home-assistant:/config"];
+      environment.TZ = "Europe/Berlin";
+      image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+      extraOptions = [
+        "--network=host"
+        "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
+      ];
     };
   };
 
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.1.1w"
+  networking.firewall.allowedTCPPortRanges = [
+    {
+      from = 100;
+      to = 65535;
+    }
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    {
+      from = 100;
+      to = 65535;
+    }
   ];
 }
