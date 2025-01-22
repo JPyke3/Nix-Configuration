94332517998bec4f5207f0662f071e93146ebf1e
JPyke3 <github@pyk.ee>
2025-01-23 08:21:26 +1000

    Rebuild: NixOS: 33

--- programs/daemon/home-assistant.nix
+++ programs/daemon/home-assistant.nix
@@ -7,7 +7,6 @@
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [
         "--network=host"
-        "--device=/dev/ttyACM0:/dev/ttyACM0" # Example, change this to match your own hardware
       ];
     };
   };
