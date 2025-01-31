a4bdc50e1864683fc83f632073a17edab4e887d2
JPyke3 <github@pyk.ee>
2025-01-31 14:46:37 +1000

    Rebuild: NixOS: 33

--- programs/daemon/sonarr.nix
+++ programs/daemon/sonarr.nix
@@ -8,4 +8,11 @@
   systemd.services.sonarr.serviceConfig = {
     UMask = "0002";
   };
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "aspnetcore-runtime-6.0.36"
+    "aspnetcore-runtime-wrapped-6.0.36"
+    "dotnet-sdk-6.0.428"
+    "dotnet-sdk-wrapped-6.0.428"
+  ];
 }
