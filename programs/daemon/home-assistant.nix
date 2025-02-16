c1489d5b9ceb6d8724a95833a55a77b207506ad4
JPyke3 <github@pyk.ee>
2025-02-17 08:12:31 +1000

    Rebuild: NixOS: 35

--- programs/daemon/home-assistant.nix
+++ programs/daemon/home-assistant.nix
@@ -1,14 +1,33 @@
 {...}: {
   virtualisation.oci-containers = {
     backend = "podman";
-    containers.homeassistant = {
-      volumes = ["home-assistant:/config"];
-      environment.TZ = "Europe/Berlin";
-      image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
-      extraOptions = [
-        "--network=host"
-        "--device=/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_24fdb0c68739ef1181b35ef454516304-if00-port0:/dev/ttyUSB0"
-      ];
+    containers = {
+      homeassistant = {
+        volumes = ["home-assistant:/config"];
+        environment.TZ = "Europe/Berlin";
+        image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
+        extraOptions = [
+          "--network=host"
+          "--device=/dev/serial/by-id/usb-Itead_Sonoff_Zigbee_3.0_USB_Dongle_Plus_V2_24fdb0c68739ef1181b35ef454516304-if00-port0:/dev/ttyUSB0"
+        ];
+      };
+      silabs-multipan = {
+        image = "ghcr.io/b2un0/silabs-multipan-docker:latest";
+        autoStart = true;
+        privileged = true;
+        network = "host";
+        extraOptions = [
+          "--cap-add=SYS_ADMIN"
+          "--cap-add=NET_ADMIN"
+        ];
+        volumes = [
+          "multipan:/data"
+        ];
+        environment = {
+          DEVICE = "/dev/ttyUSB0"; # Update this to your device path
+          BACKBONE_IF = "eth0"; # Update this to your network interface
+        };
+      };
     };
   };
 
